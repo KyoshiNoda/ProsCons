ba3885d53da3083c3d8d8b9bbe03c607b4056cbf
KyoshiNoda <kyoshisew@gmail.com>
2023-01-16 14:41:34 -0500

    added react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import { createBrowserRouter, RouterProvider, } from "react-router-dom"
+import Homepage from './pages/Homepage'
+import Login from './pages/Login'
+import SignUp from './pages/SignUp'
+import ErrorPage from './pages/ErrorPage'
+const router = createBrowserRouter([
+  {
+      path: "/",
+      element: <Homepage />,
+      errorElement: <ErrorPage />,
+  },
+  {
+      path: "/SignUp",
+      element: <SignUp />,
+      errorElement : <ErrorPage/>,
+  },
+  {
+      path: "/login",
+      element: <Login />,
+      errorElement : <Login/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ client/src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div className='flex justify-center'>
+      ERROR 404 PAGE DOESN'T EXIST!!
+    </div>
+  )
+}
+
+export default ErrorPage
