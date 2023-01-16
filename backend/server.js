16eb3df6d22cb942774d7eb3a578d135a96854ea
KyoshiNoda <kyoshisew@gmail.com>
2023-01-16 13:52:13 -0500

    added basic setup for full stack

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const app = express();
-mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "Pros-Cons"
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DB
 })
 app.listen(3001,() =>{
   console.log("listening on port 3001");
@@ -16,8 +24,9 @@ app.get('/', (req,res) =>{
   res.send("hello world");
 });
 
-app.get('/api/account/:id',(req,res) =>{
-  res.send({
-    account : req.params.id
+app.get('/api/accounts',(req,res) =>{
+  const sqlSelect = "SELECT * FROM `Pros-Cons`.accounts;"
+  db.query(sqlSelect,(err,result) =>{
+    res.send(result);
   });
 });
