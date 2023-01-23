01d753d47272530e7bbda46a7274dd4a0d11f5c7
unknown <kyoshisew@gmail.com>
2023-01-22 20:54:12 -0500

    added dynamic renders on viewList and added pages with id of pro and con

--- client/src/components/ViewList/ViewItem.jsx
+++ client/src/components/ViewList/ViewItem.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 function ViewItem(props) {
+  const link = `/ViewSingle/:${props.user_id}`
   return (
-    <a href="/ViewSingle" className="bg-cyan-500 w-40 h-40 flex justify-center items-center rounded-full overflow-hidden shadow-lg hover:bg-cyan-700">
+    <a href={link} className="bg-cyan-500 w-40 h-40 flex justify-center items-center rounded-full overflow-hidden shadow-lg hover:bg-cyan-700">
       <h1 className='text-white font-bold'>{props.name}</h1>
     </a>
   )
--- client/src/pages/ViewList.jsx
+++ client/src/pages/ViewList.jsx
@@ -4,28 +4,21 @@ import ViewItem from '../components/ViewList/ViewItem';
 import Card from '../components/Card';
 function ViewList() {
   const [viewList, setViewList] = useState([]);
-  useEffect(() => {
-    Axios.get('http://localhost:3001/api/list')
-      .then((res) => {
-        setViewList(res.data);
-        console.log(viewList);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }, []);
+  Axios.get('http://localhost:3001/api/list')
+    .then((res) => {
+      setViewList(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
   return (
     <Card>
       <div className="flex justify-center items-center gap-11 flex-wrap">
-        <ViewItem name="Apple" />
-        <ViewItem name="Netflix" />
-        <ViewItem name="SBU" />
-        <ViewItem name="Applied Visions" />
-        {/* {
+        {
           viewList.map((data) => {
-            <ViewItem name={data.name} />
+            return <ViewItem name={data.name} user_id={data.user_id} />
           })
-        } */}
+        }
       </div>
     </Card>
 
