e613c8e66507f66b3fc264606bb66fbbdc336642
KyoshiNoda <kyoshisew@gmail.com>
2023-02-06 15:27:44 -0500

    added mvc accounts

--- backend/src/DB/mySQL.js
+++ backend/src/DB/mySQL.js
@@ -1,22 +1,32 @@
+require('dotenv').config();
 const mysql = require('mysql');
-const dotenv = require('dotenv').config();
+
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB
+
 });
+db.connect((err) =>{
+  if(err){
+    console.log(err);
+  }
+})
+
+module.exports = db;
 
 
-const getAccounts = () =>{
-  const select = "SELECT * FROM `Pros-Cons`.users;"
-  const result = db.query(select,(err,result) =>{
-    return res.send(result);
-  });
 
-  return result;
-}
+// const getAccounts = () =>{
+//   const select = "SELECT * FROM `Pros-Cons`.users;"
+//   const testResult = db.query(select,(err,result) =>{
+//     return result;
+//   });
 
-module.exports ={
-  getAccounts
-}
+//   return testResult;
+// }
+
+// module.exports ={
+//   getAccounts
+// }
--- backend/src/controllers/accountController.js
+++ backend/src/controllers/accountController.js
@@ -1,8 +1,19 @@
-const accountService = require('../services/accountService');
-
-const getAllAccounts = (req,res) =>{
-  const accounts = accountService.getAccounts();
-  res.send({data : accounts})
+// const accountService = require('../services/accountService');
+const User = require('../models/userModal');
+const db = require('../DB/mySQL');
+let users = [];
+const getAllAccounts =  (req,res) =>{
+  const select = "SELECT * FROM `Pros-Cons`.users;";
+  db.query(select,(err,result) =>{
+    if(err){
+      res.status(400).send(err);
+    }
+    else{
+      res.status(200);
+      users = result;
+      res.send(users);
+    }
+  })
 }
 
 module.exports = {
